40159e334baedc18e54f42bf9c0e264d20721b64
Robel Kebede <rkebede1982@gmail.com>
2021-09-16 00:45:20 -0400

    ininital this for message.

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
     public static void main (String[] args)
     {
         System.out.println("Hello World");
+        System.out.println("Peace for Addis Ababa!")
         System.out.println("Peace for all!");
         System.out.println("Peace for Ethiopia!");
         System.out.println("Peace for Albuquerque!");
